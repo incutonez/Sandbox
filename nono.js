879058486ecc374642db5a5a8c514b0b5fc12248
jef <incutonez@hotmail.com>
2013-03-24 00:47:07 -0400

    adding in the number fields for the columns and rows

--- nono.js
+++ nono.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
+	var json = {
+		"columns": {"col0": [1, 2, 3, 4], "col1": [1, 1], "col2": [2, 4, 4, 4, 4], "col3": [10], "col4": [6, 6]},
+		"rows": {"row0": [1, 2, 3], "row1": [2, 2, 3], "row2": [10], "row3": [2, 3, 1], "row4": [1, 1]}
+	};
 	$('#nonoSize').on('change', function(event) {
 		$('#nonoGrid').children().remove();
+		$('#rowNums').children().remove();
+		$('#colNums').children('.colNums').remove();
 		var size = $(this).val();
 		for (var i = 0; i < size; i++) {
 			var row = '<div id="row' + i + '" class="row">';
@@ -8,9 +14,19 @@ $(document).ready(function() {
 				row += '<canvas class="column colHover" width="20" height="20" oncontextmenu="return false;"></canvas>';
 			}
 			row += '</div>';
+			$('#rowNums').append('<div class="rowNums"></div>');
+			$('#colNums').append('<div class="colNums"></div>');
 			$('#nonoGrid').append(row);
 		}
-		$('.row').css('width', size * 22 + 2 + 'px');
+		var rowWidth = $('.rowNums').css('width');
+		$('.rowNums').css({
+			width: '40px'
+		});
+		$('.row').css('width', size * 22 + 2 + rowWidth + 'px');
+		$('#spacer').css({
+			width: rowWidth,
+			height: '39px'
+		});
 		
 		$('.column').on('mousedown', function(event) {
 			var ctx = this.getContext('2d');
