ff863d12684ccce54909498e6ec07aac40611904
jef <incutonez@hotmail.com>
2013-05-25 12:54:48 -0400

    adding search filtering

--- web/grid/grid.js
+++ web/grid/grid.js
@@ -3,6 +3,7 @@ function sorter(a, b) {
 }
 
 $(document).ready(function() {
+	// Column sorting
 	$('.sorter').click(function(event) {
 		var $target = $(event.target);
 		$target.toggleClass('ascending');
@@ -17,5 +18,24 @@ $(document).ready(function() {
 		for (var i = 0; i < list.length; i++) {
 			list[i].parentNode.parentNode.appendChild(list[i].parentNode);
     }
+	});
+	
+	// Search filtering
+	$('#search').on('keyup', function(event) {
+		var $filter_text = $(this).val();
+		// Every time a key is pressed, we remove the filtering class because the filter will change
+		$('.filtered').removeClass('filtered');
+		if ($filter_text) {
+			// Find all divs that contain our filter text
+			$('#rows > div:contains("' + $filter_text + '")').addClass('filtered');
+			// In case we deleted some letters and no longer need to hide the row
+			$('.filtered').removeClass('hidden');
+			// Find all divs that don't contain our filter class and hide them
+			$('#rows > div:not(.filtered)').addClass('hidden');
+		}
+		// If the search text is empty, remove all hidden rows
+		else {
+			$('.hidden').removeClass('hidden');
+		}
 	});
 });
