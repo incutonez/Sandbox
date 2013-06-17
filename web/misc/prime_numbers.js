34ea88ae17594a5076f55abc4ab14fbc082a7fb6
jef <incutonez@hotmail.com>
2013-06-16 20:57:38 -0400

    updating prime_numbers

--- web/misc/prime_numbers.js
+++ web/misc/prime_numbers.js
@@ -1,22 +1,29 @@
-var primes = [];
-var A = 34;
-var B = 37;
-for (var i = A; i <= B; i++) {
-		// not a prime
-		if (i % 2 == 0) {
-				continue;
-		}
-		else {
-			var isPrime = true;
-			for (var j = 3; j < Math.ceil(Math.sqrt(i)); j++) {
-				if (i % j == 0) {
-					isPrime = false;
-					continue;
+$(document).ready(function() {
+	$('#submit').on('click', function() {
+		var A = $('#lower').val();
+		var B = $('#upper').val();
+		if (A != "" && B != "") {
+			var primes = [];
+			var primes_string = "";
+			for (var i = A; i <= B; i++) {
+				var isPrime = true;
+				for (var j = 2; j <= Math.ceil(Math.sqrt(i)); j++) {
+					if (i % j == 0) {
+						isPrime = false;
+						continue;
+					}
+				}
+				if (isPrime) {
+					primes.push(i);
+					if (primes_string != "") {
+						primes_string += ", " + i;
+					}
+					else {
+						primes_string = i;
+					}
 				}
 			}
-			if (isPrime) {
-				primes.push(i);
-			}
+			alert("You have " + primes.length + " prime numbers: " + primes_string);
 		}
-}
-alert(primes.length);
+	});
+});
