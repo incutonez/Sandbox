8d7c641d01708adf2ced58e97dca3239726c08f1
jef <incutonez@hotmail.com>
2013-06-16 20:27:46 -0400

    fixing the prime_numbers algorithm

--- web/misc/prime_numbers.js
+++ web/misc/prime_numbers.js
@@ -1,21 +1,22 @@
 var primes = [];
-var A = 11;
-var B = 19;
+var A = 34;
+var B = 37;
 for (var i = A; i <= B; i++) {
 		// not a prime
 		if (i % 2 == 0) {
 				continue;
 		}
-		else if (i % 3 == 0) {
-			continue;
-		}
 		else {
-				var myStr = Math.sqrt(i).toString();
-				// not a prime because it's a perfect square
-				if (myStr.indexOf('.') == -1) {
-						continue;
+			var isPrime = true;
+			for (var j = 3; j < Math.ceil(Math.sqrt(i)); j++) {
+				if (i % j == 0) {
+					isPrime = false;
+					continue;
 				}
+			}
+			if (isPrime) {
 				primes.push(i);
+			}
 		}
 }
 alert(primes.length);
