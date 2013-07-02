5e2773d2a999d85c900c444934a12adedf9f0c5f
jef <incutonez@hotmail.com>
2013-07-02 17:55:30 -0400

    adding a programming exercise Simple Energy wanted me to do... it just sums the numbers in a file

---
+++ web/misc/countNumbers.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+  var reader = new FileReader();
+  var digits = /[\d]+/g;
+  
+  $('#file').on('change', function(event) {
+    var file = event.target.files[0];
+    reader.onload = function(event) {
+      if (event.target.readyState == FileReader.DONE) {
+        var content = event.target.result;
+        var match = content.match(digits);
+        var sum = 0;
+        for (var i = 0; i < match.length; i++) {
+          sum += parseInt(match[i], 10);
+        }
+        $('#output').text(sum);
+      }      
+    };
+    reader.readAsText(file);
+  });
+});
