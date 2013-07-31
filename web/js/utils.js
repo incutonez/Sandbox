ccfa46492bb56f3b9e598326936d086b31731735
jef <incutonez@hotmail.com>
2013-07-31 08:53:51 -0600

    adding a long polling function to my utils file

--- web/js/utils.js
+++ web/js/utils.js
@@ -6,3 +6,19 @@ var isObjectEmpty = function(obj) {
   }
   return true;
 };
+
+// polling function... basically an example for me to keep
+/*(function poll() {
+  setTimeout(function() {
+    $.ajax({
+      url: 'index.php/api/users',
+      type: 'GET',
+      dataType: 'XML',
+      success: function(data) {
+        console.log(data);
+      },
+      complete: poll,
+      timeout: 10000  // only sets the timeout for response from the server
+    });
+  }, 10000);
+})();*/
