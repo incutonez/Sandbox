e022ceb303dfdc1267753731f59bcac74d9e174e
root <root@ip-10-250-22-197.us-west-2.compute.internal>
2013-09-04 23:27:59 +0000

    adding my Node.js app

---
+++ web/nodejs/MyApp/app/controllers/application_controller.js
@@ -0,0 +1,3 @@
+before('protect from forgery', function () {
+  protectFromForgery('cfcc7be2e902faeeca0939e7cbfaa5b60e24784b');
+});
---
+++ web/nodejs/MyApp/config/database.js
@@ -0,0 +1,26 @@
+module.exports = {
+    development: {
+        driver: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'mysql',
+        password: 'mysql123',
+        database: 'dev'
+    },
+    test: {
+        driver: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'mysql',
+        password: 'mysql123',
+        database: 'dev'
+    },
+    production: {
+        driver: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'mysql',
+        password: 'mysql123',
+        database: 'dev'
+    }
+};
