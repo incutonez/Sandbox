1df88fdad23cac81dee593fdd12b087dbfbf77b4
root <root@ip-10-250-22-197.us-west-2.compute.internal>
2013-09-06 01:37:48 +0000

    chat app seems to be working for the current user... will test for other users soon'

--- web/nodejs/MyApp/config/initializers/socketio.js
+++ web/nodejs/MyApp/config/initializers/socketio.js
@@ -2,8 +2,44 @@ var sio = require('socket.io');
 
 module.exports = function (compound) {
   var io = compound.io = sio.listen(compound.server);
+  var userId = "";
+  var users = {};
+  var msgId = 0;
 
   io.sockets.on('connection', function(socket) {
     console.log("io.sockets.on 'connection' ");
+    userId = socket.id;
+
+    socket.on('joined', function(data) {
+      var joined  = {};
+      users[userId] = joined[userId] = data.username;
+      console.log(data.username + ' joined');
+
+      socket.emit('userList', {
+        userList: users
+      });
+
+      socket.broadcast.emit('userList', {
+        userList: joined
+      });
+    });
+
+    socket.on('sndMsg', function(data) {
+      msgId++;
+      var newMsg = {
+        sender: data.sender,
+        message: data.message,
+        msgId: msgId,
+        dataUri: false
+      };
+
+      socket.emit('newMsg', newMsg);
+      socket.broadcast.emit('newMsg', newMsg);
+    });
+
+    socket.on('disconnect', function(data) {
+      console.log(username + ' disconnected');
+      delete users[userId];
+    });
   });
 }
--- web/nodejs/MyApp/public/javascripts/chat.js
+++ web/nodejs/MyApp/public/javascripts/chat.js
@@ -1,5 +1,56 @@
-var socket = io.connect('http://localhost');
-socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
+$(document).ready(function() {
+  var username = "";
+  $('#dialog').dialog({
+    modal: true,
+    closeOnEscape: false,
+    title: "Please enter your username.",
+    open: function(e, ui) {
+      $('.ui-dialog-titlebar-close').hide();
+    },
+    buttons: {
+      "Ok": function(e, ui) {
+        username = $('#username').prop('value');
+        if (username !== "") {
+          socket = io.connect('http://ec2-54-244-147-98.us-west-2.compute.amazonaws.com/');
+          socket.emit('joined', {
+            username: username
+          });
+          $('#dialog').dialog('close');
+        }
+      }
+    },
+    close: function(e, ui) {
+      $('#sndBtn').on('click', function(e) {
+        var msg = $('#message').prop('value');
+        socket.emit('sndMsg', {
+          sender: username,
+          message: msg
+        });
+      });
+
+      socket.on('userList', function(data) {
+        var users = data.userList;
+        for (var user in users) {
+          $('#userList').append('<div class="userList">' + users[user] + '</div>')
+        }
+      });
+
+      socket.on('newMsg', function(data) {
+        var sender = data.sender;
+        var msgId = data.msgId;
+        var msg = data.message;
+        var dataUri = data.dataUri;
+        var participant = "participant";
+
+        if (sender === username) {
+          participant = "you";
+        }
+        $('#messages').append('<p id="msg' + msgId + '"><span class="' + participant + '">' + sender + ': </span><span>' + msg + '</span></p>');
+
+        if (dataUri) {
+          $('#voiceArea').prop('src', dataUri);
+        }
+      });
+    }
+  });
 });
