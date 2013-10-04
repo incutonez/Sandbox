c29e9533cd0da3d55f876e80f8becb2db0afcbff
jharkay <jeffrey.harkay@twc-contractor.com>
2013-10-03 22:49:27 -0600

    updating the exchange layout a bit

--- web/exchange/test.js
+++ web/exchange/test.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+  var p = $('body').append('<p style="height: 100%; width: 100%;"></p>');
+  var winWidth = p.width();
+  $('body').remove('p');
+  var winWidthInHours = winWidth / 12;
+  var hours = 3600000;
   $.ajax({
     dataType: 'json',
     type: 'GET',
@@ -7,14 +12,29 @@ $(document).ready(function() {
       var mainDiv = "";
       for (var i = 0; i < data.length; i++) {
         var room = data[i];
-        var nameDiv = '<div><strong>' + room.name + '</strong></div>';
+        var nameDiv = '<div class="roomName"><strong>' + room.name + '</strong></div>';        
+        var open = new Date('2013-10-02T07:00:00');
+        var overallWidth = 0;
         
         for (var j = 0; j < room.info.length; j++) {
           var roomInfo = room.info[j];
-          var ps = '<p class="subject">' + roomInfo.subject + '</p>';
-          ps += '<p class="startDate">' + roomInfo.startDate + '</p>';
-          ps += '<p class="endDate">' + roomInfo.endDate + '</p>';
-          nameDiv += '<div class="roomInfo">' + ps + '</div>';
+          var nextTaken = new Date(roomInfo.startDate);
+          /* 3,600,000 because the date difference comes back in milliseconds,
+           * so divide by 1000, then divide by 60 (to get difference in minutes),
+           * then divide by 60 (to get difference in hours) and then multiply by
+           * 100 because each hour is worth 100 pixels. */
+          var diff = ((nextTaken - open) / hours) * winWidthInHours;
+          open = new Date(roomInfo.endDate);
+          var nextDiff = ((open - nextTaken) / hours) * winWidthInHours;
+          overallWidth += diff + nextDiff;
+          if (open) {
+            nameDiv += '<div style="width: ' + diff + 'px;" class="open roomInfo"></div>';
+          }
+          nameDiv += '<div class="roomInfo taken" style="width: ' + nextDiff + 'px;"></div>';
+        }
+        var diff = winWidth - overallWidth;
+        if (diff) {
+          nameDiv += '<div style="width: ' + diff + 'px;" class="open roomInfo"></div>';
         }
         mainDiv += '<div class="nameDiv">' + nameDiv + '</div>';
       }
