5f2591bf73dbc27604b16259470b80b951dc89c4
Jef <jeffrey.harkay@twc-contractor.com>
2014-04-30 10:39:51 -0600

    adding in some code that may be useful

--- ruby/author.rb
+++ ruby/author.rb
@@ -1,7 +1,11 @@
 require 'sequel'
 require 'json'
+require 'logger'
 
+logger = Logger.new($stdout)
+logger.level = Logger::DEBUG
 db = Sequel.connect('postgres://localhost/testing');
+db.logger = logger
 
 class Sequel::Model
   self.plugin :json_serializer
@@ -28,5 +32,14 @@ end
 class Book < Sequel::Model(:book)
 end
 
+#class AuthorBookTable < Sequel::Model
+#  set_primary_key [:author__id, :book__id]
+#  set_dataset db[:book, :author]
+#end
+
 author = Author.new
 author.get_author
+
+#AuthorBookTable.where(:author__id => 1, :book__author_id => 1).each do |e|
+#  puts JSON.pretty_generate(e.values)
+#end
