87edb71c2f497b3c70ddc3954202a2c8c92d712f
Jef <jeffrey.harkay@twc-contractor.com>
2014-05-02 08:44:57 -0600

    adding some more example code

--- ruby/author.rb
+++ ruby/author.rb
@@ -9,13 +9,13 @@ db.logger = logger
 
 class Sequel::Model
   self.plugin :json_serializer
-#  self.plugin :nested_attributes
+  self.plugin :nested_attributes
 end
 
 class Author < Sequel::Model(:author)
 #  set_dataset DB[:author, :book].where(:author_id => :author_id)
-  one_to_many :book, key: :author_id, primary_key: :id
-#  nested_attributes :book
+  many_to_many :book, left_key: :author_id, right_key: :book_id, :join_table => :author_book
+  nested_attributes :book
 
   def get_author
     Author.each do |e|
@@ -30,6 +30,9 @@ class Author < Sequel::Model(:author)
 end
 
 class Book < Sequel::Model(:book)
+  unrestrict_primary_key
+  many_to_many :author, left_key: :book_id, right_key: :author_id, :join_table => :author_book
+  nested_attributes :author
 end
 
 #class AuthorBookTable < Sequel::Model
@@ -37,8 +40,15 @@ end
 #  set_dataset db[:book, :author]
 #end
 
-author = Author.new
-author.get_author
+#author = Author.new(:name => "blah", :book => [{:book_id => 1}, {:book_id => 2}])
+author = Author.new(:name => "blah")
+#author.save
+#author.add_book(Book[1])
+author.save
+
+author.add_book(Book[1])
+author.add_book(Book[2])
+#author.get_author
 
 #AuthorBookTable.where(:author__id => 1, :book__author_id => 1).each do |e|
 #  puts JSON.pretty_generate(e.values)
