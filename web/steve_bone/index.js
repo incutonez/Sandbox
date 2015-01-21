93f0a42cb4e1eea5eb9a3f95092a442fa8fc1010
jef <incutonez@hotmail.com>
2015-01-20 21:39:25 -0700

    Adding some more CSS to highlight the text after the knob has turned

--- web/steve_bone/index.js
+++ web/steve_bone/index.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
   $('.soundboard-knob').on('click', function() {
-    console.log(this);
     if ($(this).hasClass('soundboard-knob-dot-clicked')) {
+      $(this).siblings('span').removeClass('highlighted');
       $(this).removeClass('soundboard-knob-dot-clicked').addClass('soundboard-knob-dot-unclicked');
     }
     else {
+      $(this).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
+        $(this).siblings('span').addClass('highlighted');
+      });
       $(this).removeClass('soundboard-knob-dot-unclicked').addClass('soundboard-knob-dot-clicked');
     }
   });
