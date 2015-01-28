85184874d3f37bfe8564cdad8e95eb7535642ba7
jef <incutonez@hotmail.com>
2015-01-27 21:00:01 -0700

    Adding the tape effect/sharpie font and getting the faders to look a little better/highlight the text once they finish

--- web/steve_bone/v2/index.js
+++ web/steve_bone/v2/index.js
@@ -1,37 +1,65 @@
 $(document).ready(function() {
   var intervalMs = 45;
   function removeFaderLights(faders) {
-    var j = 0;
+    var i = 0;
     var faderLights = faders.siblings('.fader-lights');
     var faderLightsChildren = faderLights.children();
     function myDownInterval() {
-      var child = $(faderLightsChildren[j++]);
+      var child = $(faderLightsChildren[i]);
       if (child.length) {
-        child.hide();
+        if (i < 2) {
+          child.removeClass('fader-light-red');
+        }
+        else if (i < 5) {
+          child.removeClass('fader-light-yellow');
+        }
+        else {
+          child.removeClass('fader-light-green');
+        }
+        i++;
       }
       else {
         clearInterval(intervalDownId);
       }
     }
-    var intervalDownId = setInterval(myDownInterval, intervalMs);
     faders.addClass(FADER_OFF_CLS).removeClass(FADER_ON_CLS);
+    var intervalDownId;
+    /* Set a timeout of our interval first, so we can let the fader start going
+     * down before we start removing the lights */
+    setTimeout(function() {
+      intervalDownId = setInterval(myDownInterval, intervalMs);
+    }, intervalMs);
   }
   function addFaderLights(clickedFader) {    
-    var i = 11;
+    var i = 10;
     var faderLightChildren
     var faderLights = clickedFader.siblings('.fader-lights');
     if (faderLights) {
       faderLightChildren = faderLights.children();
       function myInterval() {
-        var child = $(faderLightChildren[--i]);
+        var child = $(faderLightChildren[i]);
         if (child.length) {
-          child.show();
+          if (i > 4) {
+            child.addClass('fader-light-green');
+          }
+          else if (i > 1) {
+            child.addClass('fader-light-yellow');
+          }
+          else {
+            child.addClass('fader-light-red');
+          }
+          i--;
         }
         else {
           clearInterval(intervalId);
         }
       }
       var intervalId = setInterval(myInterval, intervalMs);
+      /* Call the interval function twice to populate two lights, so we don't
+       * see a lag in the colors lighting up */
+      myInterval();
+      myInterval();
+      clickedFader.removeClass(FADER_OFF_CLS).addClass(FADER_ON_CLS);
     }
   }
   var FADER_ON_CLS = 'fader-on';
@@ -39,16 +67,16 @@ $(document).ready(function() {
   $('.fader').on('click', function(e) {
     var clickedFader = $(this);
     var onFaders = $('.' + FADER_ON_CLS);
+    $('.tape-clicked').removeClass('tape-clicked');
     removeFaderLights(onFaders);
     if (clickedFader.is(onFaders)) {
       onFaders.addClass(FADER_OFF_CLS).removeClass(FADER_ON_CLS);
     }
     else {
-      clickedFader.removeClass(FADER_OFF_CLS).addClass(FADER_ON_CLS);
       addFaderLights(clickedFader);
-      /*clickedFader.one('transitionend', function(e) {
-        
-      });*/
+      clickedFader.one('transitionend', function(e) {
+        clickedFader.siblings('.tape').addClass('tape-clicked');
+      });
     }
   });
 });
