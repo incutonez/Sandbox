9bf23e7a1a411c5193ca325a99bf25c562331eec
Jef Harkay <jefharkay@vitalware.com>
2018-02-24 22:53:42 -0700

    updating script

--- perl/playlist_missing.pl
+++ perl/playlist_missing.pl
@@ -3,69 +3,66 @@ use strict;
 use HTML::Entities;
 use Data::Dumper;
 
-open(my $fh, '<', $ARGV[0]) or die "Can't open file $ARGV[0].";
-open(my $fh2, '<', $ARGV[1]) or die "Can't open file $ARGV[1].";
-
 my %artistsToCompare;
 my %artistsToCheck;
-my $extensions = '\.(mp3|wma|m4a)$';
-my $removeChars = '-|\(|\)|\s|\d|\.|_';
-while (my $line = <$fh>) {
-  if ($line =~ /src="D\:\\Music\\([^"]*)"/) {
-    my $str = lc $1;
-    chomp $str;
-    $str =~ s/$extensions//;
-    $str =~ s/$removeChars//g;
-    $str =~ s/reissue|digitalremaster|dc$//g;
-    my @splits = split(/\\/, decode_entities($str));
-    my $first = $splits[0];
-    my $second = $splits[1];
-    my $song = $splits[2];
-    if ($song) {
-      $song = substr($song, 0, 25);
-    }
-    if ($song && $song =~ /cd/i && $splits[3]) {
-      $song = substr($splits[3], 0, 25);
-    }
-    if ($song && $song ne '') {
-      $artistsToCompare{$first}{$second}{$song} = 1;
-    }
+my $extensions = '(1?|ph)\.(mp3|wma|m4a)$';
+my $removeChars = '\(|\)|\.|\*|\\?';
+foreach my $file (@ARGV) {
+  my $currHash = \%artistsToCompare;
+  # wpl format
+  my $patt = qq(src="D\\:\\\\Music\\\\([^"]*)");
+  my $splitExp = '\\\\';
+  open (my $fh, '<', $file) or die "Can't open file $file.\n";
+  if ($ARGV[1] && $file eq $ARGV[1]) {
+    $currHash = \%artistsToCheck;
   }
-}
-while (my $line = <$fh2>) {
-  if ($line =~ /\/storage\/3066-6539\/Music\/(.*)/) {
-    my $str = lc $1;
-    chomp $str;
-    $str =~ s/$extensions//;
-    $str =~ s/$removeChars//g;
-    $str =~ s/reissue|dc$//g;
-    my @splits = split(/\//, decode_entities($str));
-    my $first = $splits[0];
-    my $second = $splits[1];
-    my $song = $splits[2];
-    if ($song) {
-      $song = substr($song, 0, 25);
-    }
-    if ($song && $song ne '') {
-      $artistsToCheck{$first}{$second}{$song} = 1;
-    }
+  if ($file =~ /lastfm/) {
+    $patt = '(.*)';
+    $splitExp = ',,';
   }
-}
-
-foreach my $artist (sort keys %artistsToCompare) {
-  foreach my $album (sort keys %{$artistsToCompare{$artist}}) {
-    foreach my $song (sort keys %{$artistsToCompare{$artist}{$album}}) {
-      if ($artistsToCheck{$artist}{$album}) {
-        my $found = $artistsToCheck{$artist}{$album}{$song};
-        if ($found) {
-          delete $artistsToCheck{$artist}{$album}{$song};
-          #print "$found\n";
-        }
-        else {
-          print "$artist - $album - $song\n";
-        }
+  # format from Android playlist
+  elsif ($file =~ /Favorites/) {
+    $patt = qq(\\/storage\\/3066-6539\\/Music\\/(.*));
+    $splitExp = '\\/';
+  }
+  while (my $line = <$fh>) {
+    if ($line =~ /$patt/) {
+      my $str = lc $1;
+      chomp $str;
+      $str =~ s/$extensions//;
+      $str =~ s/$removeChars//g;
+      $str =~ s/-|_/ /g;
+      $str =~ s/reissue|digitalremaster|dc\d+$//g;
+      my @splits = split(/$splitExp/, decode_entities($str));
+      my $first = $splits[0];
+      my $second = $splits[1];
+      my $song = $splits[2];
+      $song = $splits[1] unless $song;
+      if ($song && $song =~ /cd|disc$/ && $splits[3]) {
+        $song = $splits[3];
+      }
+      if ($song) {
+        $song =~ s/$first\s?//;
+        $song =~ s/^\d\d\d?\s//;
+        $song =~ s/\s|:|\///g;
+        $song = substr($song, 0, 25);
+        $first =~ s/^the\s//;
+        $currHash->{$first}->{$song} = 1;
       }
     }
   }
 }
-#print Dumper(\%artistsToCheck);
+foreach my $artist (sort keys %artistsToCompare) {
+  foreach my $song (sort keys %{$artistsToCompare{$artist}}) {
+    if ($artistsToCheck{$artist}) {
+      my $found = $artistsToCheck{$artist}{$song};
+      if ($found) {
+        delete $artistsToCheck{$artist}{$song};
+      }
+      else {
+        print "$artist - $song\n";
+      }
+    }
+  }
+}
+#print Dumper(\%artistsToCompare);
