2b6578afe082001ad741b8d2eab6f45bb0fef816
Jef Harkay <jharkay@wideorbit.com>
2019-02-08 07:20:15 -0700

    - having bad titles output change to name and title

--- other_languages/music_renamer.ps1
+++ other_languages/music_renamer.ps1
@@ -1,5 +1,6 @@
 # if it includes the explicit, album version, lp version, yyyy digital remaster
 # can't have \ / : * ? " < > |
+Import-Module taglib
 $include=@("*.mp3","*.m4a", "*.wma", "*.wav", "*.m4p", "*.flac", "*.mp4")
 $badTitleChars='\(?\[?(album version|lp version|(\d\d\d\d)*\s*digital remaster|explicit|dc[^\)]*)\]?\)?'
 $replaceChars='[\\/:*?"<>|]'
@@ -9,11 +10,10 @@ $missingTitles = @()
 $badTitles = @()
 $FormatEnumerationLimit=-1
 $outFilePath = "C:\Users\jharkay\Desktop\blah_test.txt"
-#$inFilePath = "Z:\Shared Music"
-$inFilePath = "C:\Users\jharkay\Desktop\music"
+$inFilePath = "Z:\Shared Music"
+#$inFilePath = "C:\Users\jharkay\Desktop\music"
 $wStream = New-Object IO.FileStream $outFilePath,'Create','Write'
 $sWriter = New-Object System.IO.StreamWriter $wStream
-$i = 0
 echo "Starting loading files..."
 foreach($file in Get-ChildItem $inFilePath -file -recurse -include $include) 
 {
@@ -22,31 +22,31 @@ foreach($file in Get-ChildItem $inFilePath -file -recurse -include $include)
   $trackName = $objFolder.getDetailsOf($item, 21);
   $fileNameOriginal = $file.name
   if ($trackName) {
+    $trackNumber = $item.ExtendedProperty("track")
+    $extension = $file.Extension
+    $trackName = $trackName -replace $replaceChars,''
+    if ($trackNumber) {
+      if ([convert]::ToInt32($trackNumber, 10) -lt 10) {
+        $trackNumber = "0$trackNumber"
+      }
+      $trackNumber = "$trackNumber "
+    }
+    $output = "$trackNumber$trackName$extension"
     if ($trackName -match $badTitleChars) {
-      $trackName = $trackName -replace $badTitleChars,''
       $tag = [TagLib.File]::Create($file.FullName)
       $tag.tag.title = $trackName
-      $badTitles += "$fileNameOriginal`n$trackName`n"
+      $badTitles += "$fileNameOriginal`n$output`n$trackName`n"
       #$tag.save()
     }
-    else {
-      $trackNumber = $item.ExtendedProperty("track")
-      $extension = $file.Extension
-      $trackName = $trackName -replace $replaceChars,''
-      if ($trackNumber) {
-        if ([convert]::ToInt32($trackNumber, 10) -lt 10) {
-          $trackNumber = "0$trackNumber"
-        }
-        $trackNumber = "$trackNumber "
-      }
-      $output = "$trackNumber$trackName$extension"
-      if ($fileNameOriginal -ne $output) {
-        $sWriter.WriteLine("$fileNameOriginal`n$output`n")
-      }
+    else if ($fileNameOriginal -ne $output) {
+      $sWriter.WriteLine("$fileNameOriginal`n$output`n")
     }
   }
   else {
-    $missingTitles += "$($item.ExtendedProperty('artist')) - $fileNameOriginal"
+    $trackName = $fileNameOriginal -replace $badTitleChars,''
+    $tag = [TagLib.File]::Create($file.FullName)
+    $tag.tag.title = $trackName
+    $missingTitles += "$fileNameOriginal`n$trackName`n"
   }
 }
 $sWriter.WriteLine("BAD TITLES`n$($badTitles -join "`n")")
