11a7d1f270bd6f9631c5fd9d43a53f5e3143bb88
jef <incutonez@hotmail.com>
2019-02-08 17:03:39 -0700

    fixing script

--- other_languages/music_renamer.ps1
+++ other_languages/music_renamer.ps1
@@ -1,16 +1,19 @@
 # if it includes the explicit, album version, lp version, yyyy digital remaster
 # can't have \ / : * ? " < > |
+# spaces at end of title name
+# not having track number... might be able to fix that
+# noDRM not stripping out [NoDRM]-Bitch Went Nuts 
 Import-Module taglib
 $include=@("*.mp3","*.m4a", "*.wma", "*.wav", "*.m4p", "*.flac", "*.mp4")
-$badTitleChars='\(?\[?(album version|lp version|(\d\d\d\d)*\s*digital remaster|explicit|dc[^\)]*)\]?\)?'
+$badTitleChars='\s*\(?\[?(original)?(album version|lp version|(\d\d\d\d)*\s*digital remaster|explicit|dc[\d]+)\]?\)?|\s+$'
 $replaceChars='[\\/:*?"<>|]'
 $sw = [Diagnostics.Stopwatch]::StartNew()
 $objShell = New-Object -ComObject Shell.Application
 $missingTitles = @()
 $badTitles = @()
 $FormatEnumerationLimit=-1
-$outFilePath = "C:\Users\jharkay\Desktop\blah_test.txt"
-$inFilePath = "Z:\Shared Music"
+$outFilePath = "C:\Users\jef\Desktop\blah.txt"
+$inFilePath = "F:\Shared Music"
 #$inFilePath = "C:\Users\jharkay\Desktop\music"
 $wStream = New-Object IO.FileStream $outFilePath,'Create','Write'
 $sWriter = New-Object System.IO.StreamWriter $wStream
@@ -33,19 +36,26 @@ foreach($file in Get-ChildItem $inFilePath -file -recurse -include $include)
     }
     $output = "$trackNumber$trackName$extension"
     if ($trackName -match $badTitleChars) {
+      $trackName = $trackName -replace $badTitleChars,''
+      $output = "$trackNumber$trackName$extension"
       $tag = [TagLib.File]::Create($file.FullName)
-      $tag.tag.title = $trackName
+      if ($tag) {
+        $tag.tag.title = $trackName
+        #$tag.save()
+      }
       $badTitles += "$fileNameOriginal`n$output`n$trackName`n"
-      #$tag.save()
     }
-    else if ($fileNameOriginal -ne $output) {
+    elseif ($fileNameOriginal -ne $output) {
       $sWriter.WriteLine("$fileNameOriginal`n$output`n")
     }
   }
   else {
     $trackName = $fileNameOriginal -replace $badTitleChars,''
     $tag = [TagLib.File]::Create($file.FullName)
-    $tag.tag.title = $trackName
+    if ($tag) {
+      $tag.tag.title = $trackName
+      #$tag.save()
+    }
     $missingTitles += "$fileNameOriginal`n$trackName`n"
   }
 }
