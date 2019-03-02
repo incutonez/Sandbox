bbbf31fd001497891d0fee074da6652156fb48d8
Jef Harkay <jharkay@wideorbit.com>
2019-03-01 21:36:17 -0700

    updating script

--- other_languages/music_renamer.ps1
+++ other_languages/music_renamer.ps1
@@ -20,11 +20,32 @@ $badTitles = @()
 $badTitlesAndTracks = @()
 $FormatEnumerationLimit=-1
 $outFilePath = "C:\Users\jharkay\Desktop\blah2.txt"
-$inFilePath = "Z:\Shared Music"
-#$inFilePath = "C:\Users\jharkay\Desktop\music"
+#$inFilePath = "Z:\Shared Music"
+#$inFilePath = "C:\Users\jharkay\workspace\personal\applications\other_languages\test"
 $wStream = New-Object IO.FileStream $outFilePath,'Create','Write'
 $sWriter = New-Object System.IO.StreamWriter $wStream
+$shouldSave = $true
 echo "Starting loading files..."
+function doSave($file, $trackName, $output) {
+  if ($shouldSave -eq $true) {
+    $tag = [TagLib.File]::Create($file.FullName)
+    if ($tag) {
+      $tag.tag.title = $trackName
+      $tag.save()
+    }
+    if ($output) {
+      doRename($file, $output)
+    }
+  }
+}
+
+function doRename($file, $trackName) {
+  "blah $file"
+  if ($shouldSave -eq $true) {
+    Rename-Item -Path $file.FullName -NewName $trackName
+  }
+}
+
 foreach($file in Get-ChildItem $inFilePath -file -recurse -include $include) 
 {
   $directory = $file.directory
@@ -36,52 +57,52 @@ foreach($file in Get-ChildItem $inFilePath -file -recurse -include $include)
     $fileNameOriginal = $file.name
     if ($trackName) {
       $trackNumber = $item.ExtendedProperty("track")
-      $extension = $file.Extension
       $trackName = $trackName -replace $replaceChars,''
+      $extension = $file.Extension
       if ($trackNumber) {
         if ([convert]::ToInt32($trackNumber, 10) -lt 10) {
           $trackNumber = "0$trackNumber"
         }
         $trackNumber = "$trackNumber "
       }
-      $output = "$trackNumber$trackName$extension"      
+      $output = "$trackNumber$trackName$extension"
       if ($trackName -match "Track \d+") {
         if ($fileNameOriginal -match "Track \d+") {
+          #$sWriter.WriteLine("Rewriting Bad Title and Track...`n$fileNameOriginal`n$output`n")
           $badTitlesAndTracks += "$($item.ExtendedProperty('artist')) - $fileNameOriginal"
         }
         else {
           $trackName = $fileNameOriginal -replace "^\d\d\s|\.(wma|mp3)$"
           $output = "$trackNumber$trackName$extension"
-          $badTitles += "$fileNameOriginal`n$output`n$trackName`n"
+          doSave $file $trackName $output
+          $sWriter.WriteLine("Rewriting Bad Title with Track...`n$fileNameOriginal`n$output`n")
+          #$badTitles += "$fileNameOriginal`n$output`n$trackName`n"
         }
       }
       elseif ($trackName -match $badTitleChars) {
         $trackName = $trackName -replace $badTitleChars,''
-        $tag = [TagLib.File]::Create($file.FullName)
-        if ($tag) {
-          $tag.tag.title = $trackName
-          #$tag.save()
-        }
         $output = "$trackNumber$trackName$extension"
-        $badTitles += "$fileNameOriginal`n$output`n$trackName`n"
+        doSave $file $trackName $output
+        #$badTitles += "$fileNameOriginal`n$output`n$trackName`n"
+        $sWriter.WriteLine("Rewriting Bad Title...`n$fileNameOriginal`n$output`n")
       }
       elseif ($fileNameOriginal -ne $output) {
-        $sWriter.WriteLine("$fileNameOriginal`n$output`n")
+        $sWriter.WriteLine("Rewriting...`n$fileNameOriginal`n$output`n")
+        "blah $output"
+        doRename $file $output
       }
     }
     else {
       $trackName = $fileNameOriginal -replace $badTitleChars,''
-      $tag = [TagLib.File]::Create($file.FullName)
-      if ($tag) {
-        $tag.tag.title = $trackName
-        #$tag.save()
-      }
-      $missingTitles += "$fileNameOriginal`n$trackName`n"
+      $trackName = $trackName -replace $extension,''
+      doSave $file $trackName
+      $sWriter.WriteLine("Rewriting Missing Title...`n$fileNameOriginal`n$trackName`n")
+      #$missingTitles += "$fileNameOriginal`n$trackName`n"
     }
   }
 }
-$sWriter.WriteLine("BAD TITLES`n$($badTitles -join "`n")")
-$sWriter.WriteLine("MISSING TITLES`n$($missingTitles -join "`n")")
+#$sWriter.WriteLine("BAD TITLES`n$($badTitles -join "`n")")
+#$sWriter.WriteLine("MISSING TITLES`n$($missingTitles -join "`n")")
 $sWriter.WriteLine("BAD TITLES AND TRACKS`n$($badTitlesAndTracks -join "`n")")
 $sWriter.close()
 echo "Done processing files..."
