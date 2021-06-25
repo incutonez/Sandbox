57c7c707eca84f3cab65b4214fd5b27e553e0387
Jef <incutonez@hotmail.com>
2021-06-24 19:31:27 -0600

    - Adding initial drag constraints

--- typescript-node-vue/ui/src/mixins/Draggable.ts
+++ typescript-node-vue/ui/src/mixins/Draggable.ts
@@ -8,6 +8,10 @@ export default defineComponent({
       type: String,
       // Idea taken from https://stackoverflow.com/a/17206138/1253609
       default: ':scope > .jef-title'
+    },
+    dragConstrain: {
+      type: Boolean,
+      default: false
     }
   },
 
@@ -51,7 +55,7 @@ export default defineComponent({
       const oldTarget = this.position.targetEl;
       const newTarget = selector && this.$el.querySelector(selector);
       if (newTarget) {
-        newTarget.onmousedown = this.dragMouseDown;
+        newTarget.onmousedown = this._dragStart;
         newTarget.style.cursor = 'move';
         this.position.targetEl = newTarget;
       }
@@ -61,35 +65,50 @@ export default defineComponent({
       }
     },
 
-    dragMouseDown(event: IEventMouse) {
+    _dragStart(event: IEventMouse) {
       event.preventDefault();
-      // get the mouse cursor position at startup:
       this.position.currentX = event.clientX;
       this.position.currentY = event.clientY;
-      document.onmouseup = this.closeDragElement;
-      // call a function whenever the cursor moves:
-      document.onmousemove = this.elementDrag;
+      this.$el.onmousemove = this._drag;
+      this.$el.onmouseup = this._dragEnd;
     },
 
-    elementDrag(event: IEventMouse) {
-      const pos = this.position;
-      const el = this.$el;
-      event = event || window.event;
+    _drag(event: IEventMouse) {
       event.preventDefault();
-      // calculate the new cursor position:
-      pos.nextX = pos.currentX - event.clientX;
-      pos.nextY = pos.currentY - event.clientY;
-      pos.currentX = event.clientX;
+      const pos = this.position;
+      const parentEl = this.$el;
+      const top = parentEl.offsetTop;
+      const left = parentEl.offsetLeft;
+      const nextY = pos.currentY - event.clientY;
+      const nextX = pos.currentX - event.clientX;
+      let inBoundsX = true;
+      let inBoundsY = true;
+      if (this.dragConstrain) {
+        /* It's possible that our el is not the same as the targetEl, and we need to use that as our
+         * origin for if we're in bounds... but we need to still use the el when we're changing its position, as
+         * that's the parent's position */
+        const el = pos.targetEl || parentEl;
+        const rect = el.getBoundingClientRect();
+        const nextTop = rect.top - nextY;
+        const nextLeft = rect.left - nextX;
+        inBoundsY = nextTop > 0 && rect.height + nextTop < innerHeight;
+        inBoundsX = nextLeft > 0 && rect.width + nextLeft < innerWidth;
+      }
+      if (inBoundsY) {
+        pos.nextY = nextY;
+        parentEl.style.top = top - nextY + 'px';
+      }
+      if (inBoundsX) {
+        pos.nextX = nextX;
+        parentEl.style.left = left - nextX + 'px';
+      }
       pos.currentY = event.clientY;
-      // set the element's new position:
-      el.style.left = (el.offsetLeft - pos.nextX) + 'px';
-      el.style.top = (el.offsetTop - pos.nextY) + 'px';
+      pos.currentX = event.clientX;
     },
 
-    closeDragElement() {
-      /* stop moving when mouse button is released:*/
-      document.onmouseup = null;
-      document.onmousemove = null;
+    _dragEnd() {
+      this.$el.onmouseup = null;
+      this.$el.onmousemove = null;
     }
   }
 });
