10b07a244b4c2fdbf3b0abc1588e90074d19d1c0
Jef <incutonez@hotmail.com>
2022-04-23 15:59:36 -0400

    - Adding script that updates all projects to latest deps - Using published core-ui in ZeldaWorldBuilder/ui

---
+++ updateDependencies.js
@@ -0,0 +1,10 @@
+﻿import { execSync } from "child_process";
+
+const apps = ["CompanyContacts/api", "CompanyContacts/shared", "CompanyContacts/ui", "RPAN", "ZeldaWorldBuilder/api", "ZeldaWorldBuilder/ui"];
+apps.forEach((app) => {
+  console.info(`Updating: ${app}`);
+  const out = execSync("npx npm-check-updates -u", {
+    cwd: app,
+  });
+  console.log(out.toString());
+});
