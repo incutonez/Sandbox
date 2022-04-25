04141ac91f3c599ef3fc3648e4c42e95db4f366e
Jef <incutonez@hotmail.com>
2022-04-25 19:47:51 -0400

    - Trying to fix build error

--- ZeldaWorldBuilder/ui/vite.config.js
+++ ZeldaWorldBuilder/ui/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-import postcss from "ui/postcss.config.js";
+import postcss from "./postcss.config.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
