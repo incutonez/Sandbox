60837eaf44be23c99e6b0fd55f06b4fa75ae431d
Jef <incutonez@hotmail.com>
2023-10-06 21:32:43 -0400

    - Adding small script to remove guids from file names

---
+++ removeFileNameGuid.js
@@ -0,0 +1,14 @@
+import fs from "fs";
+
+let [pathToDir] = process.argv.slice(2);
+const GuidRegex = /_[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}/ig;
+if (!pathToDir) {
+  throw Error("Please specify the path!");
+}
+pathToDir = pathToDir.replace("\"", "");
+fs.readdirSync(pathToDir).forEach((file) => {
+  if (GuidRegex.test(file)) {
+    file = `${pathToDir}\\${file}`;
+    fs.renameSync(file, file.replace(GuidRegex, ""));
+  }
+});
