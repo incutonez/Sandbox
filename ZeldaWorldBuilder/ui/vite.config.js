9e990a638121be1be7fc2e4f37af7874471bf408
Jef <incutonez@hotmail.com>
2023-12-30 00:35:09 -0500

    - Attempting to fix ZeldaWorldBuilder's Jimp requirements... almost there, but now we're stuck on it thinking it's an older version of node?

--- ZeldaWorldBuilder/ui/vite.config.js
+++ ZeldaWorldBuilder/ui/vite.config.js
@@ -1,23 +1,45 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import postcss from "./postcss.config.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url)).replace(/\\/g, "\\\\");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: "/Sandbox/",
-  css: {
-    postcss,
-  },
-  resolve: {
-    alias: {
-      "ui": path.resolve(path.resolve(), "./src"),
-    },
-  },
-  build: {
-    cssCodeSplit: false,
-    assetsInlineLimit: 100000,
-    assetsDir: ".",
-  },
+	plugins: [
+		vue(),
+		nodePolyfills({
+			include: ["path", "stream", "util"],
+			exclude: ["http"],
+			globals: {
+				Buffer: true,
+				global: true,
+				process: true,
+			},
+			overrides: {
+				fs: "memfs",
+			},
+			protocolImports: true,
+		}),
+	],
+	base: "/Sandbox/",
+	css: {
+		postcss,
+	},
+	resolve: {
+		alias: {
+			ui: path.resolve(path.resolve(), "./src"),
+		},
+	},
+	build: {
+		cssCodeSplit: false,
+		assetsInlineLimit: 100000,
+		assetsDir: ".",
+	},
+	define: {
+		__dirname: `"${__dirname}"`,
+	},
 });
