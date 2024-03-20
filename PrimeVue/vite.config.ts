ce786826d4ee63eca0521c969f306166fa17e4ed
Jef <incutonez@hotmail.com>
2024-03-20 14:48:58 -0400

    fix: Adding base path for UI build

--- PrimeVue/vite.config.ts
+++ PrimeVue/vite.config.ts
@@ -4,11 +4,14 @@ import { defineConfig } from "vite";
 import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [vue(), svgLoader()],
-	resolve: {
-		alias: {
-			"@": path.resolve(path.resolve(), "./src"),
+export default defineConfig(({ mode }) => {
+	return {
+		plugins: [vue(), svgLoader()],
+		base: mode === "GitHubPages" ? "/Sandbox/" : "",
+		resolve: {
+			alias: {
+				"@": path.resolve(path.resolve(), "./src"),
+			},
 		},
-	},
+	};
 });
