f53b3271639173b8e96cb84835858becb1dba4b5
Jef <incutonez@hotmail.com>
2024-03-21 12:34:25 -0400

    - Removing test

--- API/src/main.ts
+++ API/src/main.ts
@@ -4,14 +4,6 @@ import { writeFileSync } from "fs";
 import * as path from "path";
 import { AppModule } from "src/app.module";
 
-interface test {
-	thing: string;
-}
-
-const blah: test = {
-	thing: 1,
-};
-
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
