e429733840be431ed4ac685b84cada4369ad31f0
Jef <incutonez@hotmail.com>
2024-06-09 11:10:51 -0400

    - Adding a way to filter out very old posts on LI

--- linkedIn/main.js
+++ linkedIn/main.js
@@ -2,7 +2,8 @@
  * In order to use this, go to about:debugging#/runtime/this-firefox and load the manifest.json file as the Temporary Add-on
  */
 const MatchRegex = /Viewed|Applied|Promoted/;
-const CompaniesRegex = /DataAnnotation|Veeva Systems|Aha!|HireMeFast|Team Remotely|Recruiting from Scratch|myGwork/i;
+const CompaniesRegex = /DataAnnotation|Veeva Systems|Aha!|HireMeFast|Team Remotely|Recruiting from Scratch|myGwork|Jerry/i;
+const Today = new Date();
 
 setInterval(removeJobs, 100);
 
@@ -14,6 +15,11 @@ function removeJobs() {
 	});
 	document.querySelectorAll(".job-card-container__primary-description").forEach((el) => {
 		if (el.innerHTML.match(CompaniesRegex)) {
+			el.closest(".jobs-search-results__list-item").remove();
+		}
+	});
+	document.querySelectorAll(".job-card-container__footer-item time").forEach((el) => {
+		if (Today - new Date(el.getAttribute("datetime")) > 604800000) {
 			el.closest(".jobs-search-results__list-item").remove();
 		}
 	});
