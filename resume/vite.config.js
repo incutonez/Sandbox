61df238ef6fb5ad8b10d0d54d2da43e83ecf545c
Jef <incutonez@hotmail.com>
2024-08-17 18:33:24 -0400

    feat: Attempting to add resume to GH Pages

--- resume/vite.config.js
+++ resume/vite.config.js
@@ -1,9 +1,9 @@
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { defineConfig } from "vite";
-
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(function(_a) {
+	const mode = _a.mode;
 	return {
 		plugins: [vue()],
 		base: mode === "GitHubPages" ? "/Sandbox/resume" : "",
