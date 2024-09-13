df8ba52fa79ba8b128612f541ffe4459a8b42446
Jef <incutonez@hotmail.com>
2024-09-13 17:25:44 -0400

    fix: Changing GH link in resume - Fixing HTML issue with tr not being wrapped in tbody

--- resume/src/components/SectionContactInfo.vue
+++ resume/src/components/SectionContactInfo.vue
@@ -8,56 +8,58 @@ import IconPin from "@/assets/IconPin.vue";
 
 <template>
 	<table>
-		<tr>
-			<td>
-				<IconEmail class="size-5" />
-			</td>
-			<td class="pl-3">
-				<a
-					href="mailto:jefharkay@gmail.com"
-					class="underline"
-				>jefharkay@gmail.com</a>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<IconPhone class="size-5" />
-			</td>
-			<td class="pl-3">
-				720-252-1193
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<IconPin class="size-5" />
-			</td>
-			<td class="pl-3">
-				Sterling, VA
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<IconLinkedIn class="size-5" />
-			</td>
-			<td class="max-w-36 truncate pl-3 md:max-w-full">
-				<a
-					href="https://www.linkedin.com/in/jefharkay"
-					class="text-xs underline"
-					target="_blank"
-				>https://www.linkedin.com/in/jefharkay</a>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<IconGitHub class="size-5" />
-			</td>
-			<td class="max-w-36 truncate pl-3 md:max-w-full">
-				<a
-					href="https://github.com/incutonez/Sandbox"
-					class="text-xs underline"
-					target="_blank"
-				>https://github.com/incutonez/Sandbox</a>
-			</td>
-		</tr>
+		<tbody>
+			<tr>
+				<td>
+					<IconEmail class="size-5" />
+				</td>
+				<td class="pl-3">
+					<a
+						href="mailto:jefharkay@gmail.com"
+						class="underline"
+					>jefharkay@gmail.com</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<IconPhone class="size-5" />
+				</td>
+				<td class="pl-3">
+					720-252-1193
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<IconPin class="size-5" />
+				</td>
+				<td class="pl-3">
+					Sterling, VA
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<IconLinkedIn class="size-5" />
+				</td>
+				<td class="max-w-36 truncate pl-3 md:max-w-full">
+					<a
+						href="https://www.linkedin.com/in/jefharkay"
+						class="text-xs underline"
+						target="_blank"
+					>https://www.linkedin.com/in/jefharkay</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<IconGitHub class="size-5" />
+				</td>
+				<td class="max-w-36 truncate pl-3 md:max-w-full">
+					<a
+						href="https://github.com/incutonez"
+						class="text-xs underline"
+						target="_blank"
+					>https://github.com/incutonez</a>
+				</td>
+			</tr>
+		</tbody>
 	</table>
 </template>
