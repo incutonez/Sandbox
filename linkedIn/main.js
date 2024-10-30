7892c9a9aa2e6ce23fe479275a9715914e317cf0
Jef <incutonez@hotmail.com>
2024-10-30 16:22:08 -0400

    feat: Adding Project Scaffolding - Adding a scaffolding script for a UI project... currently only Vue, but I want to add React as well

--- linkedIn/main.js
+++ linkedIn/main.js
@@ -2,7 +2,7 @@
  * In order to use this, go to about:debugging#/runtime/this-firefox and load the manifest.json file as the Temporary Add-on
  */
 const MatchRegex = /Viewed|Applied|Promoted/;
-const CompaniesRegex = /Get It Recruit|Actalent|Patterned Learning|G2i|SideRamp|DataAnnotation|Veeva Systems|Aha!|HireMeFast|Team Remotely|Recruiting from Scratch|myGwork|Jerry|RemoteWorker|ClickJobs\.io|Varsity Tutors|Ascendion/i;
+const CompaniesRegex = /Novum Global|Pragmatike|Energy Jobline|JTek Software Solutions|Software Technology Inc|Get It Recruit|Actalent|Patterned Learning|G2i|SideRamp|DataAnnotation|Veeva Systems|Aha!|HireMeFast|Team Remotely|Recruiting from Scratch|myGwork|Jerry|RemoteWorker|ClickJobs\.io|Varsity Tutors|Ascendion/i;
 const Today = new Date();
 const OneWeek = 604800000;
 const HoursAgoRegex = /(\d+) hours ago/;
---
+++ scaffold/src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+	<span class="text-red-500">Testing</span>
+</template>
