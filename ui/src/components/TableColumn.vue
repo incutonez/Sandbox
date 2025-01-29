1e6c88f456111f7d33be0cd849d049f7e817f796
Jef <incutonez@hotmail.com>
2025-01-29 15:46:13 -0500

    - Commenting out TableColumn.vue, as updates to PrimeVue have broken it

--- ui/src/components/TableColumn.vue
+++ ui/src/components/TableColumn.vue
@@ -1,15 +1,14 @@
-<template>
-	<TableColumn v-bind="getColumnProps(props)" />
-</template>
+<!--We can't create a custom wrapper column component yet.  Looks like it should be fixed in 3.48.0-->
+<!--Source: https://github.com/primefaces/primevue/issues/4940-->
 
-<script setup lang="ts">
-/**
- * We can't create a custom wrapper column component yet.  Looks like it should be fixed in 3.48.0
- * Source: https://github.com/primefaces/primevue/issues/4940
- */
-import TableColumn from "primevue/column";
-import { ITableColumn } from "@/types/table";
-import { getColumnProps } from "@/utils/table";
+<!--<template>-->
+<!--	<TableColumn v-bind="getColumnProps(props)" />-->
+<!--</template>-->
 
-const props = defineProps<ITableColumn>();
-</script>
+<!--<script setup lang="ts">-->
+<!--import TableColumn from "primevue/column";-->
+<!--import { ITableColumn } from "@/types/table";-->
+<!--import { getColumnProps } from "@/utils/table";-->
+
+<!--const props = defineProps<ITableColumn>();-->
+<!--</script>-->
