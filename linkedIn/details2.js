46afacaf3d8176bb762c98acdd6a9f7b8420e14a
Jef <incutonez@hotmail.com>
2025-02-10 13:03:53 -0500

    - Adding LI pay column if it exists

--- linkedIn/details2.js
+++ linkedIn/details2.js
@@ -1,6 +1,7 @@
 let companyName;
 let jobTitle;
 let url;
+let pay = "";
 let appliedDate = new Intl.DateTimeFormat('en-US', {
   day: '2-digit',
   month: '2-digit',
@@ -13,9 +14,16 @@ if (location.hostname === "www.indeed.com") {
   url = location.href.split('?')[0] + `?jk=${Object.fromEntries(new URLSearchParams(location.search)).jk}`;
 }
 else {
+  const skills = document.getElementsByClassName('job-details-preferences-and-skills__pill');
   const parent = document.getElementsByClassName('job-details-jobs-unified-top-card__company-name')[0];
+  for (const skill of skills) {
+    if (skill.innerText.includes("$")) {
+      pay = skill.innerText;
+      break;
+    }
+  }
   companyName = parent.getElementsByTagName("a")[0]?.innerText || parent.innerText;
   jobTitle = document.getElementsByClassName('t-24 t-bold inline')[0].innerText;
   url = location.href.split('?')[0];
 }
-navigator.clipboard.writeText(`${companyName};${jobTitle};${appliedDate};${url}`);
+navigator.clipboard.writeText(`${companyName};${jobTitle};${appliedDate};${url};${pay}`);
