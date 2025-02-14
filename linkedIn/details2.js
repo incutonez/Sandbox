011d4201c032732b0fc2fac68ae29248f31a1911
Jef <incutonez@hotmail.com>
2025-02-14 09:42:04 -0500

    - Adding pay scraping for indeed

--- linkedIn/details2.js
+++ linkedIn/details2.js
@@ -8,7 +8,14 @@ let appliedDate = new Intl.DateTimeFormat('en-US', {
   year: 'numeric'
 }).format(new Date());
 if (location.hostname === "www.indeed.com") {
+  const skills = document.getElementsByClassName('js-match-insights-provider-4pmm6z');
   const parent = document.querySelector("[data-testid='inlineHeader-companyName']") ?? document.querySelector('.jobsearch-JobInfoHeader-companyNameLink') ?? document.querySelector('.jobsearch-JobInfoHeader-companyNameSimple');
+  for (const skill of skills) {
+    if (skill.innerText.includes("$")) {
+      pay = skill.innerText;
+      break;
+    }
+  }
   companyName = parent.getElementsByTagName("a")[0]?.innerText || parent.innerText;
   jobTitle = (document.querySelector("[data-testid='jobsearch-JobInfoHeader-title'") ?? document.querySelector("[data-testid='simpler-jobTitle']")).innerText;
   url = location.href.split('?')[0] + `?jk=${Object.fromEntries(new URLSearchParams(location.search)).jk}`;
